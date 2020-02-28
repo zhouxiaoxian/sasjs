b64a35b7f9154791854b71c844ec3fbff4ffbd43
Mihajlo Medjedovic <mihajlo.kimi@gmail.com>
2020-02-28 20:42:21 +0100

    chore: first tests

--- cypress/integration/backend/backend.spec.ts
+++ cypress/integration/backend/backend.spec.ts
@@ -7,27 +7,62 @@ const adapter = new SASjs({
     pathSAS9: "/SASStoredProcess/do",
     pathSASViya: "/SASJobExecution",
     appLoc: "/Public/app",
-    serverType: "SASVIYA",
+    serverType: "SAS9",
     debug: true
 });
 
-context('Preparing', () => {
+context('Testing SAS', () => {
     it('User login', (done) => {
-        cy.visit("/SASLogon/login", {
-            onLoad: function() {
-                adapter.SASlogin("username", "password")
-                .then(res => {
-                    console.log(res);
-                    expect("test").to.include("est");
+        let userLoggedIn = false;
+
+        adapter.SASlogin("", "")
+            .then(res => {
+                if (res.includes("You have signed in") ||
+                    res.includes("User already logged in")) {
+                        userLoggedIn = true;
+                    }
+
+                expect(userLoggedIn, "Login result").to.true;
+                done();
+            }, err => {
+                console.log(err);
+                done("User not logged");
+            });
+    });
+
+    it('Should make request', (done) => {
+        let data = {
+            sometable: [
+                {firstCol: 'first col value'}
+            ]
+        }
+
+        let excpectedData = [
+            ["first col value"]
+        ]
+
+        let jsonResponse: any = null;
+
+        adapter.request("common/sendArr", data).then(
+            res => {
+                try {
+                    jsonResponse = JSON.parse(res);
+                } catch(e) {
+                    console.log(e);
+                    done("Response is not json");
+                }
+
+                // expect(JSON.stringify(jsonResponse.sometable)).to.be.equal(JSON.stringify(excpectedData));
+                
+                if (JSON.stringify(jsonResponse.sometable) === JSON.stringify(excpectedData)) {
                     done();
-                })
-                .catch(err => {
-                    console.log(err);
-                    expect("test").to.include("est");
-                    done();
-                });
-            }
-        });
-    })
+                } else {
+                    done("Response is not as excpected");
+                }
+            }, err => {
+                console.error(err);
+                done("Request failed");
+        })
+    });
 })
   
