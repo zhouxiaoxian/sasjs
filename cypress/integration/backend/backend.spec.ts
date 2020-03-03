3abd016615402a739c0078d13b56e9a400e38e37
Mihajlo Medjedovic <mihajlo.kimi@gmail.com>
2020-03-03 16:40:17 +0100

    chore: adding special chars tests

--- cypress/integration/backend/backend.spec.ts
+++ cypress/integration/backend/backend.spec.ts
@@ -290,6 +290,52 @@ context("Testing SAS", () => {
       done();
     });
   });
+
+  it("ARR, special chars(tab, CRLF, CR, UTF8)", done => {
+    testStart();
+
+    const data: any = {
+      table1: [{ col1: "\t", col2: "\r\n", col3: "\r", col4: "传/傳" }]
+    };
+    adapter.request("common/sendArr", data).then(
+      (res: any) => {
+        testFinish();
+
+        expect(res.table1[0][0], getTestExecTime()).to.not.be.undefined;
+        expect(res.table1[0][0]).to.be.equal(data.table1[0].col1);
+        expect(res.table1[0][1]).to.be.equal(data.table1[0].col2);
+        expect(res.table1[0][2]).to.be.equal(data.table1[0].col3);
+        expect(res.table1[0][3]).to.be.equal(data.table1[0].col4);
+        done();
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  });
+
+  it("OBJ, special chars(tab, CRLF, CR, UTF8)", done => {
+    testStart();
+
+    const data: any = {
+      table1: [{ col1: "\t", col2: "\r\n", col3: "\r", col4: "传/傳" }]
+    };
+    adapter.request("common/sendObj", data).then(
+      (res: any) => {
+        testFinish();
+
+        expect(res.table1[0].COL1, getTestExecTime()).to.not.be.undefined;
+        expect(res.table1[0].COL1).to.be.equal(data.table1[0].col1);
+        expect(res.table1[0].COL2).to.be.equal(data.table1[0].col2);
+        expect(res.table1[0].COL3).to.be.equal(data.table1[0].col3);
+        expect(res.table1[0].COL4).to.be.equal(data.table1[0].col3);
+        done();
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  });
 });
 
 const testStart = () => {
